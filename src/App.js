e23272341b20fc7d61705dfde11916b0feef31c0
javier <javierparra740@gmail.com>
2023-04-27 10:23:28 -0300

    version 1.0 - 27/04 - navegacion, router y card

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
+import React from 'react';
+import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom';
 import './App.css';
+import Inicio from './pages/Inicio';
+import About from './pages/About';
+
+
+class Welcome extends React.Component{
+  render(){
+    return (<h2 className='text-primary'>Hola chicos yo soy un componente de clase bienvenidos.</h2>)
+  }
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Router>
+      <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+          <Link to='/' className="navbar-brand">LOGO</Link>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <Link to='/' className='nav-link' >Inicio</Link>
+              </li>
+              <li className="nav-item">
+                <Link to='/about' className='nav-link' >About</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+        
+      <Routes>
+        <Route path='/about' Component={About} > 
+        </Route>
+        <Route path='/' Component={Inicio} >
+        </Route>
+      </Routes>
+    </Router>
+    </>
   );
 }
 
---
+++ src/pages/Inicio.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Inicio(){
+    return (
+        <>
+        <div className='h1 text-danger'>Inicio</div>
+        </>
+    )
+}
+
+export default Inicio;
